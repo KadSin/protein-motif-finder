58ac82c520e7852fa974071e05047ec46a7a082b
sina <kadkhodadeisina@gmail.com>
2022-12-22 19:50:51 +0330

    Add color to cli

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import readline from 'readline'
 import { Protein } from './protein'
 const rl = readline.createInterface(process.stdin, process.stdout)
 
-rl.question('Enter `.fasta` file full path: ', (fasta) => {
+const RESET = '\x1b[0m'
+const CYAN = '\x1b[36m'
+
+rl.question(`${ CYAN }Enter '.fasta' file full path: ${ RESET }`, (fasta) => {
 	const protein = Protein.fasta(fasta)
 
-	rl.question('Enter pattern to find indices: ', (pattern) => {
-		console.log(protein.indexOf(pattern))
+	rl.question(`${ CYAN }Enter pattern to find indices: ${ RESET }`, (pattern) => {
+		console.log('Result:', protein.indexOf(pattern))
 		rl.close()
 	})
 })
